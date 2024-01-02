23317d28ba7fb7aadecdb582a323183736c0b04c
fariez11 <69473438+fariez11@users.noreply.github.com>
2024-01-02 13:48:31 +0700

    Update type password

--- project (Penilaian Mahasiswa)/resources/views/login.blade.php
+++ project (Penilaian Mahasiswa)/resources/views/login.blade.php
@@ -67,7 +67,7 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="password" placeholder="Password">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
